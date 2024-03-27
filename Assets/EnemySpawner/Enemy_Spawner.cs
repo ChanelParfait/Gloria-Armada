300d16d25c67021ba0c9063db6088bbc53ff2221
Kaytolover123 <67986731+Kaytolover123@users.noreply.github.com>
2024-03-27 22:22:32 +1100

    enemies given initial velocity

--- Assets/EnemySpawner/Enemy_Spawner.cs
+++ Assets/EnemySpawner/Enemy_Spawner.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore;
 
 
 public class Enemy_Spawner : MonoBehaviour
 {
+    [SerializeField] private Transform cameraTransform; 
+    public enum Perspective {Top_Down, Side_On};
+    Perspective currentPerspective = Perspective.Side_On; 
     [SerializeField] private GameObject[] enemies; 
 
     [SerializeField] private Camera mainCamera; 
@@ -19,15 +23,9 @@ public class Enemy_Spawner : MonoBehaviour
     void Start()
     {
         foreach(Transform child in gameObject.transform){
-            Debug.Log(child.gameObject.name);
-            //Debug.Log(child.gameObject);
-
+            // get all spawnpoints and store them in a dict 
             spawnPoints.Add(child.gameObject.name, child.gameObject);
         }
-
-
-        //SpawnEnemy("Top_Middle", 0); 
-
     }
 
     // Update is called once per frame
@@ -35,27 +33,34 @@ public class Enemy_Spawner : MonoBehaviour
     {
         
     }
-
+    // Spawn a given amount of enemies at a certain spawn point 
     private IEnumerator SpawnEnemies(string spawnPointName, int enemyIndex, int spawnAmount){
         for(int i = 0; i < spawnAmount; i++){
             // Spawn enemy 
-            Debug.Log("Spawn"); 
             SpawnEnemy(spawnPointName, enemyIndex); 
-            // then wait _ seconds
+            // then wait 3 seconds before next spawn 
             yield return new WaitForSeconds(3);
         }
     }
 
+    //Spawn an enemy of given type(index) at a given spawn point 
     private void SpawnEnemy(string spawnPointName, int enemyIndex){
-        // given a spawn position name, find the corresponding spawn point gameobject
+        // given a spawn position name, find the corresponding spawn point gameobject from the list of spawn points
         GameObject spawnPoint;
         if (spawnPoints.TryGetValue(spawnPointName, out spawnPoint)){
             // check if given enemy index is valid 
             if(enemyIndex < enemies.Length){
                 GameObject enemy = enemies[enemyIndex]; 
-                //get movement direction 
-                // use spawn point to get the spawn position and rotation 
-                Instantiate(enemy, spawnPoint.transform.position, spawnPoint.transform.localRotation);
+
+                //get movement direction and rotation
+                Vector3 orientation = GetOrientation(spawnPoint);
+                Vector3 moveDir = GetMoveDirection(spawnPoint);
+
+                // spawn enemy at the position of given spawn point 
+                GameObject spawnedEnemy = Instantiate(enemy, spawnPoint.transform.position, Quaternion.LookRotation(orientation, Vector3.up));
+                // set initial velocity of enemy in the approriate movement direction 
+                // this may later be changed to use the enemy class or provide the enemy with a particular path //
+                spawnedEnemy.GetComponent<Rigidbody>().velocity = moveDir * 5;
             }  
         }
     }
@@ -64,10 +69,66 @@ public class Enemy_Spawner : MonoBehaviour
         Debug.Log("Trigger");
 
         if(col.tag == "SpawnTrigger"){
-            // upon colliding with trigger, retrieve spawn parameters and start spawning coroutine
+            // upon colliding with a spawn trigger, retrieve spawn parameters and start the spawning coroutine
             SpawnTrigger trigger = col.GetComponent<SpawnTrigger>();
             StartCoroutine(SpawnEnemies(trigger.spawnPointName, trigger.enemyIndex, trigger.spawnAmount));
+            // disable this collider so it only executes once
             col.enabled = false; 
         }
+    }
+
+    // get the appropriate orientation of the enemy based on its spawn point
+    // and which perpectivethe game is in 
+    private Vector3 GetOrientation(GameObject spawnPoint){
+
+        if(currentPerspective == Perspective.Top_Down){
+            if(spawnPoint.name == "Left_Bottom" || spawnPoint.name == "Right_Bottom"){
+                // face toward top of screen
+                return cameraTransform.right * -1;
+            }
+            if(spawnPoint.name == "Left_Top" || spawnPoint.name == "Right_Top"){
+                // face toward bottom of screen 
+                return cameraTransform.right;
+            }
+            if(spawnPoint.name == "Top_Right" || spawnPoint.name == "Bottom_Right"){
+                // face toward left side of screen 
+                return cameraTransform.up  * -1;
+            }
+            if(spawnPoint.name == "Top_Left" || spawnPoint.name == "Bottom_Left"){
+                // face toward right side of screen
+                return cameraTransform.up;
+            }
+
+        }
+        else if(currentPerspective == Perspective.Side_On){
+            // face towards left side of camera 
+            return cameraTransform.forward  * -1;
+
+        }
+        return new Vector3(); 
+    }
+
+    // get the appropriate movement direction of the enemy based on its spawn point
+    private Vector3 GetMoveDirection(GameObject spawnPoint){
+
+            if(spawnPoint.name == "Top_Left" || spawnPoint.name == "Top_Right"){
+                // move toward bottom of screen
+                return cameraTransform.up  * -1;
+            }
+            else if(spawnPoint.name == "Bottom_Left" || spawnPoint.name == "Bottom_Right"){
+                // move toward top of screen 
+                return cameraTransform.up;
+            }
+            else if(spawnPoint.name == "Right_Top" || spawnPoint.name == "Right_Bottom"){
+                // move toward left side of screen 
+                return cameraTransform.right * -1;
+            }
+            else if(spawnPoint.name == "Left_Top" || spawnPoint.name == "Left_Bottom"){
+                // move toward right side of screen
+                return cameraTransform.right;
+            }
+            else{
+                return new Vector3();
+            }
     }
 }
