f9464f1cb624bf521ad36afd239ec7ad552ea937
Kyaw Thiha <alexplayer153@gmail.com>
2024-04-01 11:53:18 +1100

    Miscellaneous main menu changes
    Changes include:
    
    - Settings button added
    - Existing buttons slightly changed for button presses
    - Pressing "Play" now loads a new Scene
    - Pressing "Quit" now exits the game when the project is built

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+     public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
