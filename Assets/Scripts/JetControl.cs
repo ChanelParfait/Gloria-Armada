1322fcc97d413c93ff06986beb4999bf500f4af9
Andrew Kelly <andrew.j.kelly@student.uts.edu.au>
2024-04-02 17:20:15 +1100

    Fixed git on my end

--- Assets/Scripts/JetControl.cs
+++ Assets/Scripts/JetControl.cs
@@ -36,10 +36,10 @@ public class JetControl : MonoBehaviour
         lastViewportPosition = viewport.position;
     }
 
-
     Vector3 bakedVelocity = new Vector3(1, 0, 0) * 800;
-    Vector2 virtualCursor = Vector2.right * 5; //could implement better
+    Vector2 virtualCursor = new Vector2(1, 0) * 5; //could implement better
     void FixedUpdate() {
+        //just need to move the cursor in someway
         virtualCursor.x += Input.GetAxis("Mouse X");
         virtualCursor.y += Input.GetAxis("Mouse Y");
 
@@ -47,29 +47,41 @@ public class JetControl : MonoBehaviour
         Vector3 targetDirection = viewport.localRotation * virtualCursor;
         Debug.DrawRay(transform.localPosition, targetDirection);
 
-        //We should probably do something about this
-
         Vector3 velocity = bakedVelocity + rb.velocity;
 
-        //split the inputs
+        //split the inputs (not used)
         float axisInput = Vector3.Dot(targetDirection.normalized, bakedVelocity.normalized);  //-1=reverseThrust, 1=thrusts, 0=turning
         Vector3 sideAxis = Vector3.Cross(-bakedVelocity.normalized, -viewport.forward);
         float axialInput = Vector3.Dot(targetDirection.normalized, sideAxis); //-1=left, 1,right, 0=neutral
 
-        //visual rotation
+
+        //rotation
+        targetDirection.x = Mathf.Clamp(targetDirection.x, 1, float.PositiveInfinity);
         Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
         Quaternion rotation = Quaternion.Slerp(rb.rotation, targetRotation, turnMultiplier);
 
-        //for calculating aerodynamic forces`
+        //for calculating aerodynamic forces
         float slipAngle = Quaternion.Angle(rb.rotation, rotation);
         float referenceArea = Mathf.Sin(slipAngle) * wingspan;
 
-        float drag = velocity.magnitude * referenceArea * aerodynamicDrag; //simplified drag equation
-        float normal = Mathf.Pow(velocity.magnitude, 2) * referenceArea * aerodynamicEfficiency; //simplified lift equation
-        Vector3 normalAccel = sideAxis * axialInput * normal;
+        //aerodynamic forces
+        float drag = velocity.sqrMagnitude * referenceArea * aerodynamicDrag; //simplified drag equation
+        float normal = velocity.sqrMagnitude * referenceArea * aerodynamicEfficiency; //simplified lift equation
+
+        var forwardAxis = rb.rotation * Vector3.forward;
+        var sidewardAxis = Vector3.Cross(forwardAxis, viewport.forward);
+        Vector3 dragAccel = -velocity * drag;
+        Vector3 normalAccel = sidewardAxis * Mathf.Sign(axialInput) * normal;
+        Vector3 thrustAccel = rb.rotation * Vector3.forward * thrustMultiplier;
+        Debug.DrawRay(transform.localPosition, forwardAxis * 10, Color.blue);
+        Debug.DrawRay(transform.localPosition, sidewardAxis * 10, Color.red);
+        Debug.DrawRay(transform.localPosition, normalAccel * 10, Color.blue);
+        Debug.DrawRay(transform.localPosition, sidewardAxis * 10, Color.red);
+
+
 
         //Could enforce g force limits - e.g. 10G up (KO), 2G down (pop!)
-        #region
+#region
         if(normalAccel.sqrMagnitude < negativeGLimit) {
             Quaternion upsideDown = Quaternion.LookRotation(transform.forward, -transform.up);
             //rb.rotation = Quaternion.Slerp(rb.rotation, upsideDown, turn);
@@ -78,11 +90,12 @@ public class JetControl : MonoBehaviour
             Quaternion upside = Quaternion.LookRotation(transform.forward, transform.up);
             //rb.rotation = Quaternion.Slerp(rb.rotation, upside, turn);
         }
-        #endregion
+#endregion
 
         //apply rotation here in case we clamp based on normalAccel or slipAngle
         rb.MoveRotation(rotation);
 
-        rb.AddForce(normalAccel);
+        Vector3 totalAccel = normalAccel + thrustAccel;
+        rb.AddForce(totalAccel);
     }
 }
