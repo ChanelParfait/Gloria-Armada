4a143ed85cbc2931c6e468569c06eeaf344be20f
Andrew Kelly <andrew.j.kelly@student.uts.edu.au>
2024-04-02 18:52:05 +1100

    Made movement a bit more physically based

--- Assets/Scripts/JetControl.cs
+++ Assets/Scripts/JetControl.cs
@@ -6,7 +6,8 @@ public class JetControl : MonoBehaviour
     [SerializeField] float aerodynamicEfficiency = 1f;
     [SerializeField] float aerodynamicDrag = 1f;
     [SerializeField] float wingspan = 10;
-    [SerializeField] float negativeGLimit = -20;
+    [SerializeField] float accelLimit = 50;
+    //[SerializeField] float positiveGLimit = 100;
 
     public void ResetPosition(float duration) {
         StartCoroutine(ResetPosition_async(duration));
@@ -17,8 +18,8 @@ public class JetControl : MonoBehaviour
         while(elapsedTime < duration) {
             elapsedTime += Time.deltaTime;
             float completion = elapsedTime / duration;
-            virtualCursor = Vector2.Lerp(virtualCursor, new Vector2(0, 0), completion);
-            transform.localPosition = Vector3.Lerp(transform.localPosition, target, completion);
+            virtualCursor = Vector2.Lerp(virtualCursor, new Vector2(0, 0), completion * 10);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, target, completion * 10);
             yield return null;
         }
     }
@@ -27,6 +28,7 @@ public class JetControl : MonoBehaviour
     public float thrustMultiplier = 0.1f;
 
     [SerializeField] Transform viewport;
+    [SerializeField] Transform target;
     Rigidbody rb;
     Vector3 lastViewportPosition;
 
@@ -45,57 +47,45 @@ public class JetControl : MonoBehaviour
 
         //again, could implement better with perhaps a 3D cursor but this is fine for now
         Vector3 targetDirection = viewport.localRotation * virtualCursor;
+        target.position = transform.position + targetDirection;
         Debug.DrawRay(transform.localPosition, targetDirection);
 
         Vector3 velocity = bakedVelocity + rb.velocity;
 
-        //split the inputs (not used)
-        float axisInput = Vector3.Dot(targetDirection.normalized, bakedVelocity.normalized);  //-1=reverseThrust, 1=thrusts, 0=turning
-        Vector3 sideAxis = Vector3.Cross(-bakedVelocity.normalized, -viewport.forward);
-        float axialInput = Vector3.Dot(targetDirection.normalized, sideAxis); //-1=left, 1,right, 0=neutral
-
-
         //rotation
-        targetDirection.x = Mathf.Clamp(targetDirection.x, 1, float.PositiveInfinity);
+        targetDirection.x = Mathf.Clamp(targetDirection.x, 1, float.PositiveInfinity); // stop turn arounds
         Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
         Quaternion rotation = Quaternion.Slerp(rb.rotation, targetRotation, turnMultiplier);
-
-        //for calculating aerodynamic forces
-        float slipAngle = Quaternion.Angle(rb.rotation, rotation);
-        float referenceArea = Mathf.Sin(slipAngle) * wingspan;
-
-        //aerodynamic forces
-        float drag = velocity.sqrMagnitude * referenceArea * aerodynamicDrag; //simplified drag equation
-        float normal = velocity.sqrMagnitude * referenceArea * aerodynamicEfficiency; //simplified lift equation
-
-        var forwardAxis = rb.rotation * Vector3.forward;
-        var sidewardAxis = Vector3.Cross(forwardAxis, viewport.forward);
-        Vector3 dragAccel = -velocity * drag;
-        Vector3 normalAccel = sidewardAxis * Mathf.Sign(axialInput) * normal;
-        Vector3 thrustAccel = rb.rotation * Vector3.forward * thrustMultiplier;
-        Debug.DrawRay(transform.localPosition, forwardAxis * 10, Color.blue);
-        Debug.DrawRay(transform.localPosition, sidewardAxis * 10, Color.red);
-        Debug.DrawRay(transform.localPosition, normalAccel * 10, Color.blue);
-        Debug.DrawRay(transform.localPosition, sidewardAxis * 10, Color.red);
-
-
-
-        //Could enforce g force limits - e.g. 10G up (KO), 2G down (pop!)
-#region
-        if(normalAccel.sqrMagnitude < negativeGLimit) {
-            Quaternion upsideDown = Quaternion.LookRotation(transform.forward, -transform.up);
-            //rb.rotation = Quaternion.Slerp(rb.rotation, upsideDown, turn);
-        }
-        else if (normalAccel.sqrMagnitude > 0){
-            Quaternion upside = Quaternion.LookRotation(transform.forward, transform.up);
-            //rb.rotation = Quaternion.Slerp(rb.rotation, upside, turn);
-        }
-#endregion
-
-        //apply rotation here in case we clamp based on normalAccel or slipAngle
         rb.MoveRotation(rotation);
 
-        Vector3 totalAccel = normalAccel + thrustAccel;
+        //for calculating aerodynamic forces
+        Quaternion velocityOrientation = Quaternion.LookRotation(velocity, Vector3.up);
+        float slipAngle = Quaternion.Angle(rb.rotation, velocityOrientation); //angle between wings and velocity
+        float referenceArea = Mathf.Abs(Mathf.Sin(slipAngle) * wingspan); //frontal area
+
+        var forwardAxis = rb.rotation * Vector3.forward; //from nose of plane
+        var liftAxis = Vector3.Cross(velocity.normalized, viewport.forward);
+
+        float drag = velocity.sqrMagnitude * referenceArea * aerodynamicDrag; //simplified drag equation
+
+        var normalDirection = Mathf.Sign(Vector3.Dot(forwardAxis, liftAxis)); //1=lift, -1=downforce
+        Debug.Log(forwardAxis);
+        float normal = normalDirection * velocity.sqrMagnitude * referenceArea * aerodynamicEfficiency; //simplified lift equation
+        //normal = Mathf.Clamp(normal, -accelLimit, accelLimit); //simulated stall
+
+        Vector3 dragAccel = -velocity * drag;
+        Vector3 normalAccel = liftAxis * normal;
+        Vector3 thrustAccel = rb.rotation * Vector3.forward * thrustMultiplier;
+
+        Debug.DrawRay(transform.localPosition, forwardAxis * 10, Color.blue);
+        Debug.DrawRay(transform.localPosition, liftAxis * 10, Color.red);
+        Debug.DrawRay(transform.localPosition, normalAccel, Color.cyan);
+        Debug.DrawRay(transform.localPosition, thrustAccel, Color.magenta);
+        Debug.DrawRay(transform.localPosition, velocity.normalized * 20, Color.green);
+
+
+
+        Vector3 totalAccel = normalAccel + thrustAccel + dragAccel;
         rb.AddForce(totalAccel);
     }
 }
