243b7f682425ff05e5749cfef7779d5606939ac5
Kyaw Thiha <alexplayer153@gmail.com>
2024-04-19 07:35:56 +1000

    Transition added + Intro updated
    Changes:
    
    - Scene transition prototype implemented for title and loadout Scene
    - Intro scene now uses the updated hangar

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 0.8333f;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                LoadPreviousScene();
+            }
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadPreviousScene()
+    {
+        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
