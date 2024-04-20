084207cabed56068aaef8ef4b60daf5f8f2c5f4e
Kyaw Thiha <alexplayer153@gmail.com>
2024-04-21 09:46:48 +1000

    Moar sound effects + Health system fix
    Sound effects:
    
    - Blind bomb hit sound
    - Missile hit sound
    - Scattergun hit sound
    - Sound for enemy getting hit
    - Sound for player getting hit
    
    Other changes
    
    - AdvPhys Integration scene now uses my own Health/Heart system

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -20,6 +20,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Checks if the game is paused or not when ESC is pressed, then pause or unpause
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (gameIsPaused)
@@ -33,10 +34,39 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    void Pause()
+    {
+        //Stops time and enables Pause UI
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+
+        //Disables the player's ability to control their ship
+        playerControl.enabled = false;
+
+        //Disables the HUD that includes player's life, and special bar as well as the power-up slot
+        gameHUD.enabled = false;
+
+        //Blurs the scene while paused
+        PostProcessVolume ppVolume = Camera.main.gameObject.GetComponent<PostProcessVolume>();
+        ppVolume.enabled = true;
+
+        //To be used in the Resume button to make sure the game is paused when the player resumes the game
+        gameIsPaused = true;
+
+        //Locks the cursor and makes it invisible
+        Cursor.lockState = CursorLockMode.None;
+        if (Cursor.visible == false)
+        {
+            Cursor.visible = true;
+        }
+    }
+
+    //This entire resume function practically does the opposite of the Pause function
     public void Resume()
     {
         if (gameIsPaused)
         {
+
             pauseMenuUI.SetActive(false);
             Time.timeScale = 1;
             playerControl.enabled = true;
@@ -52,30 +82,16 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0;
-        playerControl.enabled = false;
-        gameHUD.enabled = false;
-        PostProcessVolume ppVolume = Camera.main.gameObject.GetComponent<PostProcessVolume>();
-        ppVolume.enabled = true;
-        gameIsPaused = true;
-        Cursor.lockState = CursorLockMode.None;
-        if (Cursor.visible == false)
-        {
-            Cursor.visible = true;
-        }
-    }
-
     public void RestartLevel()
     {
+        //Restart current scene, and makes sure time isn't stopped when the scene loads
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void QuitToTitle()
     {
+        //Restart current scene, and makes sure time isn't stopped when the scene loads
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
--- Assets/PlayerLife.cs
+++ Assets/PlayerLife.cs
@@ -8,6 +8,8 @@ public class PlayerLife : MonoBehaviour
     public static event Action OnPlayerDamage;
     public static event Action OnPlayerHeal;
 
+    [SerializeField] private GameObject gameOverScreen;
+
     public float life, maxLife;
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,14 @@ public class PlayerLife : MonoBehaviour
             Heal(1);
             Debug.Log("Life Recovered");
         }
+
+        if (life == 0)
+        {
+            if (gameOverScreen.activeSelf == false)
+            {
+                gameOverScreen.SetActive(true);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -45,6 +55,16 @@ public class PlayerLife : MonoBehaviour
         {
             life += amount;
             OnPlayerHeal?.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "EnemyProjectile")
+        {
+            TakeDamage(2);
+            Debug.Log("Damage Taken");
+            Destroy(other.gameObject);
         }
     }
 }
