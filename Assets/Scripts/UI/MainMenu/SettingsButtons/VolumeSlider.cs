e924f750c532d09497acec599b2d57a10e0cfb09
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2024-05-26 10:03:41 +1000

    All mixers set to SFX

--- Assets/Scripts/UI/MainMenu/SettingsButtons/VolumeSlider.cs
+++ Assets/Scripts/UI/MainMenu/SettingsButtons/VolumeSlider.cs
@@ -6,23 +6,25 @@ using UnityEngine.Audio;
 
 public class VolumeSlider : MonoBehaviour
 {
-
+  
     [SerializeField] Slider slider;
     [SerializeField] AudioMixer audioMixer;
+
+    [SerializeField] string volumeType = "SFX_Volume";
     // Start is called before the first frame update
     void Start()
     {
-        SetVolume(PlayerPrefs.GetFloat("Saved_SFX_Volume", 0.5f));
-        RefreshSlider(PlayerPrefs.GetFloat("Saved_SFX_Volume", 0.5f));
+        SetVolume(PlayerPrefs.GetFloat("Saved_" + volumeType, 0.5f));
+        RefreshSlider(PlayerPrefs.GetFloat("Saved_" + volumeType, 0.5f));
         
     }
 
     public void SetVolume(float volume){
         //RefreshSlider(volume);  
-        Debug.Log("Volume: " + volume);
+        //Debug.Log("Volume: " + volume);
         if (volume == 0) volume = 0.0001f;
-        audioMixer.SetFloat("SFX_Volume", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("Saved_SFX_Volume", volume);
+        audioMixer.SetFloat(volumeType, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Saved_" + volumeType, volume);
     }
 
     public void RefreshSlider(float volume){
